9f8884b34247e763e09dc1ff4cfc1c1f7ae0def6
bisho <bishoptim453@gmail.com>
2022-09-08 08:15:31 +1000

    Change server URL

--- src/rest/CategoryClient.jsx
+++ src/rest/CategoryClient.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "/api/finance",
+  baseURL: "http://10.0.0.118:8080/api/finance",
   headers: {
     "Content-type": "application/json"
   }
