3ea2153e39c65c73e547e90fc58bb519ed0217c0
bisho <bishoptim453@gmail.com>
2022-09-07 11:59:36 +1000

    Revert "Add charts to display aggregate data"
    This reverts commit 0230bc5c

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState, useEffect} from 'react';
-import TransactionForm from './components/TransactionForm';
-import CategoryClient from './rest/CategoryClient';
-<<<<<<< HEAD
-=======
-import Transactions from './components/Transactions';
->>>>>>> fb965404fb39c1a1e96aa9232c048479ffcc17b0
-
-
 
 function App() {
-
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    CategoryClient.get('/get-categories').then((response) => {
-      setCategories(response.data);
-    });
- }, []);
-
-
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
         <img src={logo} className="App-logo" alt="logo" />
-        <div class="forms">
-          <TransactionForm categories={categories}/>
-        </div>
-=======
-        <div class="Forms">
-          <TransactionForm categories={categories}/>
-        </div>
-        <div class="transactions">
-          <Transactions/>
-        </div>
->>>>>>> fb965404fb39c1a1e96aa9232c048479ffcc17b0
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
