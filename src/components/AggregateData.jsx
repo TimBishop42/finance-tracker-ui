24ff4f48f6febfff48e6e356105f5c211c730719
bisho <bishoptim453@gmail.com>
2022-09-08 08:30:22 +1000

    Change chart to use grouping

--- src/components/AggregateData.jsx
+++ src/components/AggregateData.jsx
@@ -1,6 +1,6 @@
 import RestClient from '../rest/CategoryClient';
 import React, { useState, useEffect } from 'react';
-import { VictoryBar, VictoryChart, VictoryTheme, VictoryStack } from 'victory';
+import { VictoryBar, VictoryChart, VictoryTheme, VictoryStack, VictoryGroup } from 'victory';
 
 export default function Transactions() {
 
@@ -16,7 +16,13 @@ export default function Transactions() {
 
     return (
         <VictoryChart theme={VictoryTheme.material} width={800} domainPadding={10}>
+            <VictoryGroup 
+            horizontal 
+            offset={5} 
+            style={{ data: { width: 6 } }} 
+            colorScale={["brown", "tomato", "gold"]}>
         {aggregateData.map(vals => <VictoryBar data={vals.categoryValues} x="category" y="value" />)}
+        </VictoryGroup>
         </VictoryChart>
     )
 }
