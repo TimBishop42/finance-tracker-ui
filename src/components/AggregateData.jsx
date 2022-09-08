2a1603181b39bf4078161120d6eb8fdec159fbe1
bisho <bishoptim453@gmail.com>
2022-09-08 11:07:15 +1000

    Change chart to use grouping

--- src/components/AggregateData.jsx
+++ src/components/AggregateData.jsx
@@ -1,10 +1,21 @@
 import RestClient from '../rest/CategoryClient';
 import React, { useState, useEffect } from 'react';
-import { VictoryBar, VictoryChart, VictoryTheme, VictoryStack, VictoryGroup } from 'victory';
+import { VictoryBar, VictoryChart, VictoryTheme, VictoryStack, VictoryGroup, VictoryAxis, VictoryLabel } from 'victory';
 
 export default function Transactions() {
 
     const [aggregateData, setAggregateData] = useState([]);
+
+    const chartTheme = {
+        axis: {
+            style: {
+                tickLabels: {
+                    // this changed the color of my numbers to white
+                    fill: 'white',
+                },
+            },
+        },
+    };
 
     useEffect(() => {
         RestClient.get('/get-summary-months').then((response) => {
@@ -15,14 +26,41 @@ export default function Transactions() {
 
 
     return (
-        <VictoryChart theme={VictoryTheme.material} width={800} domainPadding={10}>
-            <VictoryGroup 
-            horizontal 
-            offset={5} 
-            style={{ data: { width: 6 } }} 
-            colorScale={["brown", "tomato", "gold"]}>
-        {aggregateData.map(vals => <VictoryBar data={vals.categoryValues} x="category" y="value" />)}
-        </VictoryGroup>
+        <VictoryChart theme={VictoryTheme.material} width={1100} height={2000} domainPadding={20}>
+            <VictoryAxis
+                tickLabelComponent={<VictoryLabel dy={0} dx={10} angle={55} />}
+                style={{
+                    axis: {
+                        stroke: 'white'  //CHANGE COLOR OF X-AXIS
+                    },
+                    tickLabels: {
+                        fill: 'white', //CHANGE COLOR OF X-AXIS LABELS
+                        fontSize: 20
+                    },
+                    grid: {
+                        stroke: 'white', //CHANGE COLOR OF X-AXIS GRID LINES
+                        strokeDasharray: '7',
+                    }
+                }}
+            />
+            <VictoryGroup
+                horizontal
+                offset={25}
+                style={{ data: { width: 20 } }}
+                colorScale={["blue", "white", "red", "green", "purple"]}>
+                {aggregateData.map(vals =>
+                    <VictoryBar
+                        data={vals.categoryValues}
+                        x="category"
+                        y="value"
+                        labels={({ datum }) => `Amount: ${datum.value}`}
+                        style={{
+                            // data: { fill: "tomato", opacity: 0.7 },
+                            labels: { fontSize: 14, fill: "white" },
+                            // parent: { border: "1px solid #ccc" }
+                        }}
+                    />)}
+            </VictoryGroup>
         </VictoryChart>
     )
 }
